ba90f6aa6c47daff186fbd6c19167328eac4f56d
lex-c <chlochlo.lexa@gmail.com>
2020-10-18 00:52:40 -0400

    fix route

--- longing/routing.py
+++ longing/routing.py
@@ -1,6 +1,5 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
 import main.routing as rout
 
 application = ProtocolTypeRouter({
