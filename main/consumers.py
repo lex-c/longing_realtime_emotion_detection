112dda741a03e5de6b0944f244b82b79b8b421af
lex-c <chlochlo.lexa@gmail.com>
2020-10-18 01:09:57 -0400

    fix route

--- main/consumers.py
+++ main/consumers.py
@@ -4,7 +4,7 @@ import cv2
 import io
 import base64
 import numpy as np
-from . import views
+# from . import views
 
 # smile_cascade = cv2.CascadeClassifier('main/static/main/haarcascade_smile.xml')
 # image_url = None
