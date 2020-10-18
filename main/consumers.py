28c432859600305923617e91f84933e539bfb881
lex-c <chlochlo.lexa@gmail.com>
2020-10-17 23:28:39 -0400

    change camsocket

--- main/consumers.py
+++ main/consumers.py
@@ -2,17 +2,18 @@ from channels.generic.websocket import WebsocketConsumer
 import json
 
 class CamConsumer(WebsocketConsumer):
-    def websocket_connect(self, event):
+    def connect(self):
         self.accept()
         self.send(text_data=json.dumps({ 
             'message': 'something'
          }))
     
-    def websocket_receive(self, data):
+    def disconnect(self, text_data):
+        pass
+    
+    def receive(self, data):
        text_json = json.loads(data)
        message = data['message']
        print(message)
     
-    def websocket_disconnect(self, text_data):
-        pass
 
