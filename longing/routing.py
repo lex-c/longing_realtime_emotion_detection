2c4a265f197e7fccca141140edfc8fa985db47f5
lex-c <chlochlo.lexa@gmail.com>
2020-10-18 00:44:09 -0400

    add reqs

--- longing/routing.py
+++ longing/routing.py
@@ -1,10 +1,10 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-import main.routing
+import wc
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
-        URLRouter(main.routing.websocket_urlpatterns)
+        URLRouter(wc.routing.websocket_urlpatterns)
     )
 })
