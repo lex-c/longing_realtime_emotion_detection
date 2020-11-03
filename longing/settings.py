72ff143166bd4e993963b15119512b3dd134b95c
lex-c <chlochlo.lexa@gmail.com>
2020-11-03 00:35:04 -0500

    configure settings for deploy

--- longing/settings.py
+++ longing/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bzt1!^_1m-*^$4xzv#jn1mdsa+vmy$qeikr%e%qua%c5$a&xm&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'longing-feelings.herokuapp.com']
 
